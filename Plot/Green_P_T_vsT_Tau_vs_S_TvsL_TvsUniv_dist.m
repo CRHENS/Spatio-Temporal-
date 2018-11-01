de408efb958bc1e12d1396862f7a7697d2431492
CRHENS <44646392+CRHENS@users.noreply.github.com>
2018-11-01 16:33:19 +0530

    Add files via upload

---
+++ Plot/Green_P_T_vsT_Tau_vs_S_TvsL_TvsUniv_dist.m
@@ -0,0 +1,14 @@
+clear all;
+load Degree_Dist_T.mat;
+path(pathdef)
+addpath('.\Plot\Fig3g-l');
+addpath('.\Plot\Fig4a-c');
+addpath('.\Plot\Fig4d-f');
+addpath('.\Plot\Fig6i-k');
+plot_ptau_tau_theta_negative;
+figure;
+plot_tau_vs_S_green;
+figure
+Green_plot_T_vs_L
+figure;
+Green_M_E_Universal_Dist;
---
+++ Plot/Red_R2_P_T_vsT_Tau_vs_S_TvsL_TvsUniv_dist.m
@@ -0,0 +1,15 @@
+clear all;
+load Degree_Dist_T.mat;
+path(pathdef)
+addpath('.\Plot\Fig3g-l');
+addpath('.\Plot\Fig4a-c');
+addpath('.\Plot\Fig4d-f');
+addpath('.\Plot\Fig6i-k');
+plot_ptau_tau_theta_positive;
+figure;
+plot_tau_vs_S_red;
+figure;
+Red_plot_T_vs_L
+figure;
+Red_R_2_Universal_Dist; %%%% R_2
+%Red_P_Universal_Dist; %%%% P
