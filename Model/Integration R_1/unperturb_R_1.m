276931600ddbd6a7ce53ffc9d78538cbc8baa368
CRHENS <44646392+CRHENS@users.noreply.github.com>
2018-11-01 17:19:53 +0530

    Add files via upload

--- Model/Integration R_1/unperturb_R_1.m
+++ Model/Integration R_1/unperturb_R_1.m
@@ -2,10 +2,10 @@ function[ystart1]=unperturb_R_1(yinit,timestep,w)
 max_diff_steady_state=1;
 %%%%%%%% Without perturbation %%%%%%%
  op=odeset('Abstol',1e-11,'Reltol',1e-11);
-    fprintf('timetaken=%d',timestep)
-    k=1;
+  %%  fprintf('timetaken=%d',timestep)
+   %% k=1;
     z1=[];   tt=0.0;
-while(max_diff_steady_state>10^-9)
+while(max_diff_steady_state>10^-10)
            tic
        %    for yy=1:k
  [yt_unperb, wy_unperb] = ode45(@R_1_model,[tt tt+timestep],yinit,op);
@@ -15,7 +15,7 @@ while(max_diff_steady_state>10^-9)
  %z1=[z1;yt_unperb, wy_unperb];
       yinit=double(unperb_dat(end,1:end)); 
       tt=double(yt_unperb(end));
-       k=k+1;
+    %   k=k+1;
        toc
       %^      end
 end
