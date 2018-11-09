3593f6957c76cea0e9093dcfae625af507f18f0d
CRHENS <44646392+CRHENS@users.noreply.github.com>
2018-11-09 08:55:41 +0530

    Add files via upload

--- Model/Integration R_1/Integration_R_1dynamics.m
+++ Model/Integration R_1/Integration_R_1dynamics.m
@@ -1,3 +1,5 @@
+% Copyrigth to Chittaranjan Hens, Uzi Harush and Baruch's lab
+
 %clear all
  par_1=[ 1]; 
  par_2=[ 1];
@@ -8,8 +10,6 @@ for i=1
     mm=1; Nosc=m1*n1; 
     w=ones(1,m1*n1);
 R=1;B=1; 
- %load par_1.mat;
- %load par_2.mat;
    alpha=par_1(i); %% parameter
    h1=par_2(i);    %% parameter
   if i==1
@@ -38,9 +38,6 @@ node=1:1:50; %%Perturbation in first 500 nodes
 % %%%%%%%%%%%%%%%%%%% With  perturbation %%%%%%%%%%%%%%%%%%%%
 [dt_pert1]=perturb_R_1(ystart1,node_num,m1,n1,time_perb,w,perb_percent);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  file_name=sprintf('dt_pert_MM_2000sf_h_%g_alpha%g_node%g.mat',h1,alpha,node_num);
-%  save(file_name,'dt_pert1'); 
- %dt_pert1=[];
  eta=0.7;
 %%%%%%%%%%%%%%%%% Transient time %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [nn]=transient_measure(dt_pert1,node_num,A,eta);
@@ -64,8 +61,6 @@ transienttime1=transienttime';
  deg_perb_new=deg_perb1(1:end);
   xx=[deg_perb_new;  node_dist_perb_new; transienttime_new];
   xx=xx';
-%  %dt_pert1=[];  xx=[];
-   %file_name=sprintf('trans_R_1_2_h%g_alpha%g.mat',h1,alpha);
    file_name=sprintf('Degree_Dist_T.mat');
    save(file_name,'xx'); 
    clear;
